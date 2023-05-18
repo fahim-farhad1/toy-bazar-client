e90380853422aa7ff2b9cd0f4ed1e1b451337810
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2023-05-18 11:22:59 +0600

    Create file and manage folder structure

---
+++ src/Pages/Home/Banner/HomePageBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePageBanner = () => {
+    return (
+        <div>
+            <h2>This is home page banner!</h2>
+        </div>
+    );
+};
+
+export default HomePageBanner;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePageBanner from '../Banner/HomePageBanner';
+
+const Home = () => {
+    return (
+        <div>
+            <HomePageBanner></HomePageBanner>
+        </div>
+    );
+};
+
+export default Home;
