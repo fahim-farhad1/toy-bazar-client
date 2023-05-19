264a10128687aeb74501b8a393bd4c8164b4d35f
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2023-05-19 23:51:45 +0600

    modal setup

--- src/Pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/Pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -10,13 +10,36 @@ const ShopByCategory = () => {
       .then((res) => res.json())
       .then((data) => setToys(data.slice(0, 5)));
   }, [toys]);
-//------------Model Section----------------
-  const handelModal = (_id) =>{
-    console.log(_id);
-  }
+  //------------Model Section----------------
+  const handelModal = (_id) => {
+    <>
+      {/* The button to open modal */}
+      {/* <label htmlFor="my-modal" className="btn">open modal</label> */}
+
+      {/* Put this part before </body> tag */}
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
+            congratulations random Internet user!
+          </h3>
+          <p className="py-4">
+            You've been selected for a chance to get one year of subscription to
+            use Wikipedia for free!
+          </p>
+          <div className="modal-action">
+            <label htmlFor="my-modal" className="btn">
+              Yay!
+            </label>
+          </div>
+        </div>
+      </div>
+    </>;
+  };
   return (
     <div className="my-5">
-        <p className="md:text-4xl text-center font-bold text-orange-500">Shop by category</p>
+      <p className="md:text-4xl text-center font-bold text-orange-500">
+        Shop by category
+      </p>
       <Tabs>
         <TabList className="text-center pt-5">
           <Tab>Marvel</Tab>
@@ -40,8 +63,35 @@ const ShopByCategory = () => {
                     <h2 className="card-title">{toy.toyName}</h2>
                     <h2 className="card-title">{toy.price}</h2>
                     <h2 className="card-title">{toy.rating}</h2>
+                    {/* Modal Section */}
                     <div className="card-actions justify-end">
-                      <button className="btn btn-primary" onClick={() => handelModal(toy._id)}>View Details</button>
+                      {/* The button to open modal */}
+                      <label htmlFor="my-modal" className="btn hover:bg-orange-700 bg-orange-500">
+                        open modal
+                      </label>
+
+                      {/* Put this part before </body> tag */}
+                      <input
+                        type="checkbox"
+                        id="my-modal"
+                        className="modal-toggle"
+                      />
+                      <div className="modal">
+                        <div className="modal-box">
+                          <h3 className="font-bold text-lg">
+                            Congratulations random Internet user!
+                          </h3>
+                          <p className="py-4">
+                            You've been selected for a chance to get one year of
+                            subscription to use Wikipedia for free!
+                          </p>
+                          <div className="modal-action">
+                            <label htmlFor="my-modal" className="btn bg-orange-500 hover:bg-orange-700">
+                              Yay!
+                            </label>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
